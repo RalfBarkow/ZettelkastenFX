a44892c24788dfcb5edb51dbdba7870767895a88
Ralf Barkow <ralf.barkow@me.com>
2019-10-09 17:41:02 +0200

    Add ZettelkastenUnmarshaller (WIP)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module ch.dreyeck.zettelkastenfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml.bind;
 
-    opens ch.dreyeck.zettelkastenfx to javafx.fxml;
+    opens ch.dreyeck.zettelkastenfx to javafx.fxml, java.xml.bind;
     exports ch.dreyeck.zettelkastenfx;
+
 }
