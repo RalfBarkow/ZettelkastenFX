09e2e4a12201517293a2470746ae293de8b0d544
Ralf Barkow <ralf.barkow@me.com>
2019-10-10 12:17:19 +0200

    Package with JAXB class defined in a module must be open to at least java.xml.bind module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module ch.dreyeck.zettelkastenfx {
     requires javafx.fxml;
     requires java.xml.bind;
 
-    opens ch.dreyeck.zettelkastenfx to javafx.fxml, java.xml.bind;
+    opens ch.dreyeck.zettelkastenfx to javafx.fxml;
     exports ch.dreyeck.zettelkastenfx;
 
+    opens ch.dreyeck.zettelkasten.xml to java.xml.bind;
+    exports ch.dreyeck.zettelkasten.xml;
 }
