6ba0c6bb8cb8d4ddac77a4f49c1e9a29a33e6d2d
Ralf Barkow <ralf.barkow@me.com>
2019-10-12 18:56:58 +0200

    ZettelkastenViewController (loadZettelkasten) listView Zettel (WIP)

--- src/main/java/ch/dreyeck/zettelkastenfx/ZettelkastenViewController.java
+++ src/main/java/ch/dreyeck/zettelkastenfx/ZettelkastenViewController.java
@@ -1,9 +1,10 @@
 package ch.dreyeck.zettelkastenfx;
 
+import ch.dreyeck.zettelkasten.xml.Zettel;
 import ch.dreyeck.zettelkasten.xml.Zettelkasten;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,17 +22,13 @@ public class ZettelkastenViewController implements Initializable {
     private final ObjectProperty<Zettelkasten> zettelkasten = new SimpleObjectProperty<>(new Zettelkasten());
 
     @FXML
-    private ListView<String> listView;
+    private ListView<Zettel> listView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        ObservableList<String> items = listView.getItems(); // erst getItems !!!
-        items.add("One");
-        items.add("Two");
-        items.add("Three");
-        items.add("Four");
-        items.add("Five");
+        // TODO: Binding
+
     }
 
     @FXML
@@ -40,6 +37,9 @@ public class ZettelkastenViewController implements Initializable {
             final Unmarshaller unmarshaller =
                     JAXBContext.newInstance(Zettelkasten.class).createUnmarshaller();
             zettelkasten.set((Zettelkasten) unmarshaller.unmarshal(new File("zknFile.xml")));
+
+            // TODO: (values)
+            listView.setItems(FXCollections.observableList(zettelkasten.getValue().getZettel()));
 
         } catch (final JAXBException e) {
             e.printStackTrace();
