a82344ab291c211c3e14a7c13e5fb9990eeaba10
Ralf Barkow <>
2020-11-20 12:12:27 +0100

    test(ZettelkastenViewControllerTest): generate test

---
+++ src/test/java/ch/dreyeck/zettelkastenfx/ZettelkastenViewControllerTest.java
@@ -0,0 +1,24 @@
+package ch.dreyeck.zettelkastenfx;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ZettelkastenViewControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void loadZettelkasten() {
+    }
+}
