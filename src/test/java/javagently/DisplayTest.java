fd6a69013fa0f53e93a02e9423cba369b7a0f1d4
Ralf Barkow <>
2020-12-04 11:31:22 +0100

    test(DisplayTest): testing the Time class

---
+++ src/test/java/javagently/DisplayTest.java
@@ -0,0 +1,67 @@
+package javagently;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DisplayTest {
+
+    DisplayTest() {
+        DisplayTest.Time time, newTime;
+        Display d = new Display ("Testing times");
+        d.prompt("Hour",11);
+        d.prompt("Min",30);
+        d.prompt("add minutes",15);
+        for (; ;) {
+            d.ready("\nPress ready when hours and mins entered");
+            int hour = d.getInt("Hour");
+            int min = d.getInt("Min");
+            time = new DisplayTest.Time(hour, min);
+            d.println("Time is " + time);
+            time.addTime(d.getInt("add minutes"));
+            d.println("and is now " + time);
+        }
+    }
+
+    // Put the Time class here
+    class Time {
+        int hour;
+        int min;
+
+        Time (int h, int m) {
+            hour = h;
+            min = m;
+        }
+
+        Time (int mins) {
+            hour = mins / 60;
+            min = mins % 60;
+        }
+
+        Time (double t) {
+            hour = (int) t;
+            min = (int) t*100 - hour*100;
+        }
+
+        Time (TimeDisplay.Time t) {
+            hour = t.hour;
+            min = t.min;
+        }
+        void addTime (int m) {
+            min +=m;
+            hour+= min / 60;
+            min %= 60;
+        }
+
+        boolean lessThan (TimeDisplay.Time t) {
+            return hour < t.hour | (hour==t.hour & min < t.min);
+        }
+
+        public String toString () {
+            return hour + ":" + min + " ";
+        }
+
+    }
+
+    public static void main (String [] args) {
+        new TimeDisplay ();
+    }
+}
