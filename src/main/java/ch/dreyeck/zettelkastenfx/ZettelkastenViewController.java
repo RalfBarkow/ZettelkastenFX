5e97347091860d8c6ffda9689f3d2bd027e5bead
Ralf Barkow <>
2021-03-15 17:28:15 +0100

    refactor(ZettelkastenView): loadZknFileXML()

--- src/main/java/ch/dreyeck/zettelkastenfx/ZettelkastenViewController.java
+++ src/main/java/ch/dreyeck/zettelkastenfx/ZettelkastenViewController.java
@@ -1,6 +1,5 @@
 package ch.dreyeck.zettelkastenfx;
 
-import ch.dreyeck.zettelkasten.input.ZipFilteredReader;
 import ch.dreyeck.zettelkasten.xml.Zettel;
 import ch.dreyeck.zettelkasten.xml.Zettelkasten;
 import jakarta.xml.bind.JAXBContext;
@@ -9,15 +8,18 @@ import jakarta.xml.bind.Unmarshaller;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 
+import java.io.File;
+
 public class ZettelkastenViewController {
 
     private final ObjectProperty<Zettelkasten> zettelkasten = new SimpleObjectProperty<>(new Zettelkasten());
+
+    @FXML
     public Button btnLoad;
 
     @FXML
@@ -33,11 +35,11 @@ public class ZettelkastenViewController {
     }
 
     @FXML
-    void loadZettelkasten(final ActionEvent event) {
+    void loadZknFileXML() {
         try {
             final Unmarshaller unmarshaller =
                     JAXBContext.newInstance(Zettelkasten.class).createUnmarshaller();
-            zettelkasten.set((Zettelkasten) unmarshaller.unmarshal(new ZipFilteredReader("/Users/rgb/rgb~Zettelkasten/Zettelkasten-Dateien/#303-DesktopFrame/rgb.zkn3", "/Users/rgb/tmp/ziptest").filteredExpandZipFile(zipEntry -> zipEntry.getName().equals("zknFile.xml"))));
+            zettelkasten.set((Zettelkasten) unmarshaller.unmarshal(new File("zknFile.xml")));
             zettelListView.setItems(FXCollections.<Zettel>observableList(zettelkasten.getValue().getZettel()));
         } catch (final JAXBException e) {
             e.printStackTrace();
