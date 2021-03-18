a6fea4408a0cd966e9aa0e3945dd00d42ffe77ed
Ralf Barkow <>
2021-03-18 15:35:55 +0100

    feat(Demo07HoldSnapshot): from József Hegedüs https://youtu.be/CjEDmJMLEGE?t=568

---
+++ src/main/java/ch/dreyeck/essence/Demo07HoldSnapshot.java
@@ -0,0 +1,37 @@
+package ch.dreyeck.essence;
+
+import nz.sodium.Cell;
+import nz.sodium.Stream;
+import nz.sodium.StreamSink;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Demo07HoldSnapshot {
+    public static void main(String[] args) throws IOException {
+
+        StreamSink<String> input = new StreamSink<>();
+
+        Cell<Integer> outputCell =
+                input.filter(x -> isNaturalNumber(x)).map(x -> str2Integer(x)).hold(0);
+        Stream<Integer> outputStream = input.filter(x -> x.equals("snapshot")).snapshot(outputCell);
+
+        outputCell.listen(x -> System.out.println("outputCell: " + x));
+        outputStream.listen(x -> System.out.println("outputStream: " + x));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String str = br.readLine();
+            input.send(str);
+        }
+    }
+
+    private static boolean isNaturalNumber(String str) {
+        return str.matches("\\d+");
+    }
+
+    private static Integer str2Integer(String str) {
+        return new Integer(Integer.parseInt(str));
+    }
+}
