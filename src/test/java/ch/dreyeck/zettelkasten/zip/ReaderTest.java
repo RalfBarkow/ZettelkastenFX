883d287fb522e68f3f52d6c769066dfaf334efcb
Ralf Barkow <>
2021-03-29 09:12:08 +0200

    refactor: move input/ZettelkastenFileTest to zip/ReaderTest

--- src/test/java/ch/dreyeck/zettelkasten/zip/ReaderTest.java
+++ src/test/java/ch/dreyeck/zettelkasten/zip/ReaderTest.java
@@ -1,17 +1,16 @@
-package ch.dreyeck.zettelkasten.input;
+package ch.dreyeck.zettelkasten.zip;
 
 import ch.dreyeck.zettelkasten.xml.Zettelkasten;
-import ch.dreyeck.zettelkasten.zip.Reader;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-class ZettelkastenFileTest {
+class ReaderTest {
 
     @Test
-    void getZknFileXML() {
+    void filter() {
         ObjectProperty<Zettelkasten> ZETTELKASTEN_OBJECT_PROPERTY = new SimpleObjectProperty<>(new Zettelkasten());
         Reader reader = new Reader("/Users/rgb/rgb~Zettelkasten/Zettelkasten-Dateien/rgb.zkn3", ZETTELKASTEN_OBJECT_PROPERTY);
         ZETTELKASTEN_OBJECT_PROPERTY = reader.filter(zipEntry -> zipEntry.getName().equals("zknFile.xml"));
